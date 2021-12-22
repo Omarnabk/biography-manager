175311589609d4be8087adba1efc8013ef1b870c
Wesam Alnabki <mnab@unileon.es>
2021-12-22 16:38:28 +0100

    Fix user data files path

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 invitation_link_base = 'https://mws.api.itu.int/t/mws-users/#/accept/'
 profile_photo_url_root = 'https://www.itu.int/itad/Biography-Self-service/'
 
-bio_save_path = 'D:/Biography-Self-service/'
-# bio_save_path = '/var/www/html/Biography-Self-service/'
+# bio_save_path = 'D:/Biography-Self-service/'
+bio_save_path = '/var/www/html/Biography-Self-service/'
 
 ALLOWED_PHOTO_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
