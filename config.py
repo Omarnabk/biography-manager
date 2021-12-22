d05f4671fbce99c44dde618d841c405651355550
Wesam Alnabki <mnab@unileon.es>
2021-12-22 18:56:32 +0100

    Fix photo path

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 invitation_link_base = 'https://mws.api.itu.int/t/mws-users/#/accept/'
 profile_photo_url_root = 'https://www.itu.int/itad/BiographySelfservice/speaker_data_files/'
 
-bio_save_path = 'D:/BiographySelfservice/speaker_data_files/'
-# bio_save_path = '/var/www/html/BiographySelfservice/speaker_data_files/'
+# bio_save_path = 'D:/BiographySelfservice/speaker_data_files/'
+bio_save_path = '/var/www/html/BiographySelfservice/speaker_data_files/'
 
 ALLOWED_PHOTO_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
