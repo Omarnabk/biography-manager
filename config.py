c0531d2bf937448332f8ca6576b7b656b0adfaed
Wesam Alnabki <mnab@unileon.es>
2022-01-01 09:30:07 +0100

    Add user local link

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 invitation_link_base = 'https://mws.api.itu.int/t/mws-users/#/accept/'
 profile_photo_url_base = 'https://www.itu.int/itad/BiographySelfservice/speaker_data_files/'
-profile_url_base = 'https://mws.api.itu.int/t/mws-users/#/user/'
+# profile_url_base = 'https://mws.api.itu.int/t/mws-users/#/user/'
+profile_url_base = 'http://localhost:8101/mws-users/#/user/'
 # bio_save_path = 'D:/BiographySelfservice/speaker_data_files/'
 bio_save_path = '/var/www/html/BiographySelfservice/speaker_data_files/'
 
